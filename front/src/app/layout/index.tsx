804f6afc3d48de64df9e83ebd4c0604bf3237968
argen <amukhamedaliuulu@nurtelecom.kg>
2023-02-24 10:26:51 +0600

    theme colors

--- front/src/app/layout/index.tsx
+++ front/src/app/layout/index.tsx
@@ -1,16 +1,21 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from 'widgets/ui'
+import { useTheme } from 'shared/lib/providers'
 
 export const Layout = () => {
+		const { theme } = useTheme()
+
 		return (
 				<>
-						<Header/>
-						<main className="main">
-								<Outlet/>
-						</main>
-						<footer className="footer">
-								footer
-						</footer>
+						<div className={ `app ${ theme }` }>
+								<Header/>
+								<main className="main">
+										<Outlet/>
+								</main>
+								<footer className="footer">
+										footer
+								</footer>
+						</div>
 				</>
 		)
 }
