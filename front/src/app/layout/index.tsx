eb4df5f7df574fe7ba85e080110f6ed0be7a282b
argen <amukhamedaliuulu@nurtelecom.kg>
2023-02-28 11:27:26 +0600

    remove icon

--- front/src/app/layout/index.tsx
+++ front/src/app/layout/index.tsx
@@ -2,14 +2,11 @@ import { Outlet } from 'react-router-dom'
 import { Header } from 'widgets/ui'
 import { useTheme } from 'shared/lib/providers'
 
-import SvgIcon from 'app/assets/cld-cloud-computer-network-svgrepo-com.svg'
-
 export const Layout = () => {
 	const { theme } = useTheme()
 	return (
 		<div className={`app ${theme}`}>
 			<Header/>
-			<SvgIcon width={20} height={'auto'}/>
 			<main className="main">
 				<Outlet/>
 			</main>
